b10d5397908b691f264bd233b75374e8d5801f55
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2021-07-26 20:20:18 +0530

    Add, Delete, Edit Contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddContactComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Contact } from '../Contact';
+import { ContactService } from '../contact.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  contactList: Contact[]
+  
+  constructor(private router: Router, private contactService: ContactService) { }
+
+  ngOnInit() {
+    this.contactService.getContacts().subscribe(data => {
+      this.contactList = data
+    })
+  }
+
+  // Add New Contact
+  addContact(): void {
+    this.router.navigate(['addContact'])
+  }
+
+  // Modify Contact
+  editContact(contact: Contact): void {
+    localStorage.removeItem("editContactId")
+    localStorage.setItem("editContactId", contact.id.toString())
+    this.router.navigate(['editContact'])
+  }
+
+  // Delete Contact
+  deleteContact(contact: Contact): void {
+    this.contactService.deleteContact(contact.id).subscribe(data => {
+      this.contactList = this.contactList.filter(c => c!== contact)
+    })
+  }
+
+}
